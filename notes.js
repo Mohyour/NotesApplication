c76be23cd8890f3f1502831cc3604f7814a605d1
mohyour <mohyour01@gmail.com>
2016-08-17 13:33:13 +0100

    Added search method and note model

--- notes.js
+++ notes.js
@@ -1,11 +1,18 @@
+class Notes {
+    constructor(authors, notes) {
+        this.authors = author
+        this.note_contents = note_contents
+        this.note_id = note_id
+    }
+}
+
 class NotesApplication {
     constructor(author) {
         this.author = author;
         this.note_list = [];
     }
 
-    create(note_content) {
-        this.note_content = note_content;
+    create(note_content, author) {
         this.note_list.push(note_content);
     }
 
@@ -21,7 +28,23 @@ class NotesApplication {
     }
 
     get(note_id) {
+        if (note_id >= this.note_list.length) {
+            return "Id not available";
+        }
         return String(this.note_list[note_id]);
     }
 
+    search(search_text) {
+        for (var i = 0; i < this.note_list.length; i++) {
+            if( this.note_list[i].indexOf(search_text) >= 0){
+                console.log("Note ID: ", i);
+                console.log(this.note_list[i]);
+                console.log("By ", this.author);
+                console.log(" ");
+            }
+            else {
+                return "Note not found"
+            }
+        }
+    }
 }
