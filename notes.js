6db312d2faaa54c913d117dbc8130263dca90971
mohyour <mohyour01@gmail.com>
2016-08-17 12:25:37 +0100

    Added list and get method

--- notes.js
+++ notes.js
@@ -1,16 +1,31 @@
 class NotesApplication {
-    constructor(author){
+    constructor(author) {
         this.author = author;
         this.note_list = [];
     }
 
-    notes(){
+    notes() {
         return this.note_list;
     }
 
     create(note_content) {
         this.note_content = note_content;
         this.note_list.push(note_content);
+    }
+
+
+    listNotes() {
+        for (var i = 0; i < this.note_list.length; i++) {
+            console.log("Note ID: ", i);
+            console.log(this.note_list[i]);
+            console.log(" ");
+
+        }
+    console.log("By ", this.author);
+    }
+
+    get(note_id) {
+        return String(this.note_list[note_id]);
     }
 
 }
